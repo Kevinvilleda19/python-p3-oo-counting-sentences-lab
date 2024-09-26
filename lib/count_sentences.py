155195a9d84f40b343b2410e1de046e76a743958
Kevin <kvilleda10566@gmail.com>
2024-09-26 15:35:17 -0400

    Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self,value = ''):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+
+  def count_sentences(self):
+      sentences = re.split(r'[.!?]+\s*', self.value)
+
+        # Filter out empty sentences (like trailing spaces)
+      non_empty_sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+
+        # Return the count of non-empty sentences
+      return len(non_empty_sentences)
